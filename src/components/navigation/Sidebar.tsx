e4d73fcf5f0d91104e6a361b4869dbe6e0cc488d
Kamalesh-Suresh-Kumar <230701138@rajalakshmi.edu.in>
2026-07-24 14:35:48 +0530

    Version 1 and Version 2 completed

--- src/components/navigation/Sidebar.tsx
+++ src/components/navigation/Sidebar.tsx
@@ -59,57 +59,44 @@ import {
     FaMicrochip,
     FaSatelliteDish
 } from "react-icons/fa";
-
 export default function Sidebar() {
     return (
         <aside>
-
             <h2>Navigation</h2>
-
             <ul>
-
                 <li>
                     <FaHome />
                     <span>Lobby</span>
                 </li>
-
                 <li>
                     <FaLaptopCode />
                     <span>Engineering Laboratory</span>
                 </li>
-
                 <li>
                     <FaBrain />
                     <span>Memory Archive</span>
                 </li>
-
                 <li>
                     <FaTrophy />
                     <span>Achievement Hall</span>
                 </li>
-
                 <li>
                     <FaFolderOpen />
                     <span>Document Vault</span>
                 </li>
-
                 <li>
                     <FaComments />
                     <span>Communication Center</span>
                 </li>
-
                 <li>
                     <FaMicrochip />
                     <span>Skill Processor</span>
                 </li>
-
                 <li>
                     <FaSatelliteDish />
                     <span>Mission Control</span>
                 </li>
-
             </ul>
-
         </aside>
     );
 }
